4f6ac898b6a7061a13206398a944ebcfa71f2f20
zyf <1528337826@qq.com>
2020-09-21 00:05:25 +0800

    完善目录

--- src/main/java/com/zzl/english/config/controller/EnglishWordController.java
+++ src/main/java/com/zzl/english/config/controller/EnglishWordController.java
@@ -1,13 +1,16 @@
 package com.zzl.english.config.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/word")
 public class EnglishWordController {
 	
 	
-	
-//	public String findWord() {
-//		
-//	}
+	@GetMapping("/test")
+	public String findWord() {
+		return "test word";
+	}
 }
